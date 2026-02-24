e4d4ce718a50b09531cf7f768d78cf7aa04d8bfb
FourBasic <alex.dunn@live.ca>
2026-02-23 21:49:15 -0500

    ^^ \__/

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,24 @@
 #include <Arduino.h>
 #include "Thermocouple/Thermocouple.h"
 #include "AwfulPID.h"
+#include "DutyCycle.h"
 
 #define PIN_IN_THERMOCOUPLE A0
 #define PIN_OUT_HEATER 0
 
 Thermocouple tc;
-//Debounce period;
+DutyCycle dc;
 
 // Configure PID for an output in seconds correlating to the duty cycle of the heater.
-// 100 second duty cycle period, duty cycle of 0-100%
+// duty cycle of 0-100% * 10
 // 10 second PID period
 AwfulPID pid;
-const PIDConfiguration pidCfg {10, 0, 100, false, 3, 50};
+const PIDConfiguration pidCfg {10000, 0, 1000, false, 30, 5};
 const PIDParameters pidParam {9.0, 0.0, 0.0};
 int temperatureSetpoint;
 
 void setup() {
-    tc.setup(0,1);
-    //period.setup(0);
+    tc.setup(TC_TYPE_K, 1, 10, 200);
     pid.setConfig(pidCfg);
     pid.setParam(pidParam);
 }
@@ -30,10 +30,11 @@ void loop() {
     int temp;    
     temp = tc.update(uVolt);
 
-    int dutyCycle = pid.update(PID_ENABLE, temp, temperatureSetpoint);
+    float dutyCycle;
+    dutyCycle = float(pid.update(PID_ENABLE, temp, temperatureSetpoint) / 10);
+    bool heatCycle;
+    heatCycle = dc.update(true, 60000, dutyCycle);
 
-    //period.update(!period.getState, )
-    // Need to create a new function in timer just for duty cycle
-    
+    digitalWrite(PIN_OUT_HEATER, heatCycle);
 }
 
