950a36030a74565b3ab6d182518d149112d9495c
FourBasic <alex.dunn@live.ca>
2026-03-15 11:44:25 -0400

    table data for temperature sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 #include <Arduino.h>
-#include "Thermocouple/Thermocouple.h"
+#include "Sensor555/Sensor555.h"
 #include "AwfulPID.h"
 #include "DutyCycle.h"
+#include "GeneralFunctions.h"
 #include "Adafruit_GFX.h" 
 #include "Adafruit_TFTLCD.h"
 #include "MCUFRIEND_kbv.h"
 
-#define PIN_IN_THERMOCOUPLE A0
-#define PIN_OUT_HEATER 0
+#define PIN_IN_RTD A5
+#define PIN_OUT_HEATER 13
 
 #define LCD_CS A3 // Chip Select goes to Analog 3
 #define LCD_CD A2 // Command/Data goes to Analog 2
@@ -49,21 +50,32 @@ struct IntegerField {
   int val;
 };
 
-Thermocouple tc;
+Sensor555 rtd;
+TableDataPoint rtdLookupTable[8] 
+{{241,110},
+{253,105},
+{278,96},
+{330,84},
+{424,70},
+{520,62},
+{730,50},
+{2015,24}};
 
 TimerOnDelay screenRefresh;
 MCUFRIEND_kbv tft;
 IntegerField field_sp {.prop={.pos={.x=5, .y=5}, .color=CYAN, .bg=BLACK, .font=3, .label="SP:", .labelColor=WHITE, .labelOffs=0, .unit="C", .padding = 5}, .base=1, .val=0};
-IntegerField field_temp {.prop={.pos={.x=5, .y=50}, .color=CYAN, .bg=BLACK, .font=3, .label="PV:", .labelColor=WHITE, .labelOffs=0, .unit="C", .padding = 5}, .base=10, .val=0};
-IntegerField field_heat {.prop={.pos={.x=5, .y=100}, .color=CYAN, .bg=BLACK, .font=3, .label="OUT:", .labelColor=WHITE, .labelOffs=0, .unit="%", .padding = 5}, .base=1, .val=0};
+IntegerField field_temp {.prop={.pos={.x=5, .y=35}, .color=CYAN, .bg=BLACK, .font=3, .label="PV:", .labelColor=WHITE, .labelOffs=0, .unit="C", .padding = 5}, .base=1, .val=0};
+IntegerField field_heat {.prop={.pos={.x=5, .y=65}, .color=CYAN, .bg=BLACK, .font=3, .label="OUT:", .labelColor=WHITE, .labelOffs=0, .unit="%", .padding = 5}, .base=1, .val=0};
 
 // Configure PID for an output in seconds correlating to the duty cycle of the heater.
 // duty cycle of 0-100% * 10
 // 10 second PID period
 AwfulPID pid;
-const PIDConfiguration pidCfg {2000, 0, 1000, false, 30, 5};
-const PIDParameters pidParam {1.0, 1.0, 0.0};
+const PIDConfiguration pidCfg {2000, 0, 1000, true, 30, 5};
+const PIDParameters pidParam {1.0, 0.0, 0.0};
+
 DutyCycle dc;
+
 int SP_temp;
 
 void initializeFieldProperties (struct FieldProperties* prop) {
@@ -82,7 +94,7 @@ void updateIntegerField(struct IntegerField* field, int val) {
   int pX_val = pos->x + prop->labelOffs;
   // Backgound previous value
   tft.setCursor(pX_val, pos->y);  
-  tft.setTextColor(prop->bg);  
+  tft.setTextColor(prop->bg);
   tft.print(field->val);
   tft.setCursor(tft.getCursorX() + prop->padding, pos->y);
   tft.print(prop->unit);
@@ -97,9 +109,13 @@ void updateIntegerField(struct IntegerField* field, int val) {
 }
 
 void setup() {
-  //Serial.begin(9600);
+  Serial.begin(9600);
 
-  tc.setup(TC_TYPE_K, 1, 10, 200);
+  digitalWrite(PIN_OUT_HEATER, HIGH);
+  pinMode(PIN_OUT_HEATER, OUTPUT);
+
+  pinMode(PIN_IN_RTD, INPUT);
+  rtd.setup(5000, -0.070528, 145.43, 9999);
   
   pid.setConfig(pidCfg);
   pid.setParam(pidParam);
@@ -110,38 +126,36 @@ void setup() {
   initializeFieldProperties(&field_sp.prop);
   initializeFieldProperties(&field_temp.prop);
   initializeFieldProperties(&field_heat.prop);
+  
+  digitalWrite(PIN_OUT_HEATER, HIGH);
+  pinMode(PIN_OUT_HEATER, OUTPUT);
+
   delay(1000);
 }
-
+    
 void loop() {
-  SP_temp = 62; 
+  SP_temp = 1900;
 
-  long tcScaled;
-  tcScaled = map(1, 0, 1023, 0, 5000000);
-  int uVolt;
-  uVolt = int(constrain(tcScaled, -32000,32000));
-  //uVolt = map(analogRead(PIN_IN_THERMOCOUPLE), 0, 1023, 0, 5000000);
+  //long tcScaled;
+  // Vin = (Vref(5V)/1024)*ADC(1023) = 4.995117V
+  // Vin = (Vref(1.1V)/1024)*ADC(1023) = 1.098926V
 
-  int temp;    
-  temp = tc.update(uVolt);
+  float temp;    
+  temp = rtd.update(digitalRead(PIN_IN_RTD));
+  temp = scaleFloatByTable(rtd.getPulseLength(), rtdLookupTable, 8);
 
-  float dutyCycle;
-  dutyCycle = float(pid.update(PID_ENABLE, temp, SP_temp) / 10);
-  bool heatCycle;
-  heatCycle = dc.update(true, 60000, dutyCycle);
+  float dutyCycle = pid.update(PID_ENABLE, temp, SP_temp);
+  dutyCycle = 16.0;
+  
+  bool heatCycle = dc.update(true, 30000, dutyCycle);
+  //digitalWrite(PIN_OUT_HEATER, !heatCycle);
 
-  //digitalWrite(PIN_OUT_HEATER, heatCycle);
 
-  //text_sp.text = String(temperatureSetpoint);
-  //tft.println(temperatureSetpoint);
-
-  //delay(200);
   if (screenRefresh.update(!screenRefresh.getTimerDone(), 500)) {
     updateIntegerField(&field_sp, SP_temp);
     updateIntegerField(&field_temp, temp);
     updateIntegerField(&field_heat, dutyCycle);
   }
-
 
 }
 
